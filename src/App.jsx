2a4c60c33f2ee516a8ee9f76f64c93f49d37d6e0
19nfyZ76 <paltatyana1@gmail.com>
2025-08-28 22:51:54 +0200

    Add Dynamic Routing elements and fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import './App.css'
 import Header from "./components/Header.jsx";
 import MainBlock from "./components/MainBlock.jsx";
 import Footer from "./components/Footer.jsx";
-
+import {useState} from "react";
 
 function App() {
-
+    const [hero, setHero] = useState("luke");
 
     return (
         <>
-            <Header />
-            <MainBlock/>
+            <Header hero={hero}/>
+            <MainBlock hero={hero} setHero={setHero}/>
             <Footer/>
         </>
     )
